b249c2362c1a3239f33622360e6a4ac2b49edba0
NightMoonCat <495982617@qq.com>
2017-03-29 10:51:33 +0800

    添加跳转工具类MFGT文件和闪屏后的欢迎界面

---
+++ SuperWeChat/src/cn/moon/superwechat/ui/GuideActivity.java
@@ -0,0 +1,14 @@
+package cn.moon.superwechat.ui;
+
+import android.os.Bundle;
+
+import cn.moon.superwechat.R;
+
+public class GuideActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
---
+++ SuperWeChat/src/cn/moon/superwechat/utils/MFGT.java
@@ -0,0 +1,49 @@
+package cn.moon.superwechat.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.moon.superwechat.R;
+import cn.moon.superwechat.ui.GuideActivity;
+import cn.moon.superwechat.ui.LoginActivity;
+import cn.moon.superwechat.ui.MainActivity;
+
+/**
+ * Created by Moon on 2017/3/16.
+ */
+
+public class MFGT {
+    public static void startActivity(Activity activity, Class cls) {
+        activity.startActivity(new Intent(activity, cls));
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void startActivity(Activity activity, Intent intent) {
+        activity.startActivity(intent);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void finish(Activity activity) {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    public static void gotoMain(Activity activity) {
+        startActivity(activity, MainActivity.class);
+    }
+    public static void gotoGuide(Activity activity) {
+        startActivity(activity, GuideActivity.class);
+    }
+
+
+    public static void gotoLogin(Activity activity,int requestCode) {
+        startActivityForResult(activity, new Intent(activity,LoginActivity.class),requestCode);
+    }
+
+
+    public static void startActivityForResult(Activity activity, Intent intent, int requestCode) {
+        activity.startActivityForResult(intent, requestCode);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+}
